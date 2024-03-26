83331b08a7ea9331fb3f6e98032f6e3578179c75
Andrew Yang <andy.tubeee@gmail.com>
2024-03-26 17:16:49 -0400

    Initial Commit

---
+++ eskateboard-app/eskateboard_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  eskateboard_appApp.swift
+//  eskateboard-app
+//
+//  Created by Andrew Yang on 2024-03-26.
+//
+
+import SwiftUI
+
+@main
+struct eskateboard_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
